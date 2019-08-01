a74679c7121b75de7162ebccb8c1ffedaf45557b
兴 湛 <869070197@qq.com>
2019-08-01 20:17:56 +0800

    no message

--- config/dev.env.js
+++ config/dev.env.js
@@ -1,5 +1,5 @@
 module.exports = {
   NODE_ENV: '"development"',
   ENV_CONFIG: '"dev"',
-  BASE_API: '"http://172.100.15.252:8080/tax_caishui_admin"'
+  BASE_API: '"http://tax-caishui-admin.int.anniu-inc.com"'
 }
