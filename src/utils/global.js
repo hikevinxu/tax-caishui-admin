71f5a2d249991afbb8e2c01e62742abf428d5871
安牛 <zhenhua.xu@kongapi.com>
2019-08-22 18:41:12 +0800

    提交测试

--- src/utils/global.js
+++ src/utils/global.js
@@ -12,7 +12,7 @@ const global = {
   ],
   pageUrlList: [
     {name: '公司详情页', id: "main/home/queryService/company"},
-    {name: '服务详情页', id: "main/home/queryService/servicer"}
+    {name: '服务详情页', id: "main/home/queryService/service"}
   ]
 }
 export default global
