fa916a089ada9edb6b270d9372fbe0725cd4c4aa
安牛 <zhenhua.xu@kongapi.com>
2019-10-18 10:38:06 +0800

    提交正式

--- src/views/merchants/serviceList.vue
+++ src/views/merchants/serviceList.vue
@@ -7,9 +7,9 @@
       <el-select class="filter-item" v-if="secondServiceCodeList.length > 0" v-model="secondServiceCode" placeholder="请选择关联类目" @change="secondServiceCodeChange"  style="width: 200px">
         <el-option v-for="(item,index) in secondServiceCodeList" :key="item+index" :label="item.name" :value="item.code"/>
       </el-select>
-      <el-select class="filter-item" v-if="thirdServiceCodeList.length > 0" v-model="thirdServiceCode" placeholder="请选择关联类目" @change="thirdServiceCodeChange"  style="width: 200px">
+      <!-- <el-select class="filter-item" v-if="thirdServiceCodeList.length > 0" v-model="thirdServiceCode" placeholder="请选择关联类目" @change="thirdServiceCodeChange"  style="width: 200px">
         <el-option v-for="(item,index) in thirdServiceCodeList" :key="item+index" :label="item.name" :value="item.code"/>
-      </el-select>
+      </el-select> -->
       <el-select class="filter-item" v-model="listQuery.companyId" filterable remote reserve-keyword placeholder="请输入公司全称" style="width: 350px" @change="companyChange" :remote-method="searchCompanyList" :loading="loading">
         <el-option v-for="item in companyList" :key="item.name" :label="item.name" :value="item.id"></el-option>
       </el-select>
@@ -329,23 +329,25 @@ export default {
       }
     },
     secondServiceCodeChange(val) {
-      this.thirdServiceCode = ''
-      this.thirdServiceCodeList = []
-      if(val && val != ''){
-        let param = {
-          parentCode: val
-        }
-        serviceTypeList(param).then(res => {
-          if(res.code == 0){
-            if (res.data.length > 0) {
-              this.thirdServiceCodeList = res.data
-            } else {
-              this.listQuery.serviceCode = val
-              this.getList()
-            }
-          }
-        })
-      }
+      this.listQuery.serviceCode = val
+      this.getList()
+      // this.thirdServiceCode = ''
+      // this.thirdServiceCodeList = []
+      // if(val && val != ''){
+      //   let param = {
+      //     parentCode: val
+      //   }
+      //   serviceTypeList(param).then(res => {
+      //     if(res.code == 0){
+      //       if (res.data.length > 0) {
+      //         this.thirdServiceCodeList = res.data
+      //       } else {
+      //         this.listQuery.serviceCode = val
+      //         this.getList()
+      //       }
+      //     }
+      //   })
+      // }
     },
     thirdServiceCodeChange(val) {
       this.listQuery.serviceCode = val
